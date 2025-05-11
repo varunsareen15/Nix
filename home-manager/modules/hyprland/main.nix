cd086a50ce15dd8cf73ad270ed2b2da2c835df09
Varun Sareen <varunsareen15@gmail.com>
2025-05-11 00:20:23 -0500

    Added some animations

--- home-manager/modules/hyprland/main.nix
+++ home-manager/modules/hyprland/main.nix
@@ -22,7 +22,19 @@
       ];
 
       animations = {
-        enabled = false;
+        enabled = true;
+
+        
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+          "windows,     1, 7,  myBezier"
+          "windowsOut,  1, 7,  default, popin 80%"
+          "border,      1, 10, default"
+          "borderangle, 1, 8,  default"
+          "fade,        1, 7,  default"
+          "workspaces,  1, 6,  default"
+        ];
       };
 
       input = {
@@ -43,11 +55,11 @@
 
       decoration = {
         blur = {
-          enabled = false;
+          enabled = true;
 	};
 
 	shadow = {
-	  enabled = false;
+	  enabled = true;
 	};
 
 	active_opacity = 1.0;
