1ebc061a332f27a98c1aa84609c9461276db60ea
Varun Sareen <varunsareen15@gmail.com>
2025-05-22 19:32:23 -0400

    fixed bluetooth

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -2,6 +2,11 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
   };
 
   services.blueman.enable = true;
